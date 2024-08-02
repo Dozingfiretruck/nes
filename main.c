3a94cd894d2d9f214affdf752d81837c1d95d7cb
Dozingfiretruck <1473454180@qq.com>
2024-08-02 23:56:45 +0800

    fix:release版本打印

--- main.c
+++ main.c
@@ -1,50 +1,42 @@
 /*
- * MIT License
+ * Copyright 2023-2024 Dozingfiretruck
  *
- * Copyright (c) 2023 Dozingfiretruck
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 
 #include "nes.h"
 
+
 int main(int argc, char *argv[]){
     if (argc == 2){
-        const char *nes_file_path = argv[1];
+        const char* nes_file_path = argv[1];
         size_t nes_file_path_len = strlen(nes_file_path);
         if (memcmp(nes_file_path+nes_file_path_len-4,".nes",4)==0 || memcmp(nes_file_path+nes_file_path_len-4,".NES",4)==0){
-            nes_printf("nes_file_path:%s\n",nes_file_path);
+            printf("nes_file_path:%s\n",nes_file_path);
             nes_t* nes = nes_load_file(nes_file_path);
             if (!nes){
-                nes_printf("nes load file fail\n");
+                printf("nes load file fail\n");
                 goto error;
             }
             nes_run(nes);
             nes_unload_file(nes);
             return 0;
         }else{
-            nes_printf("Please enter xxx.nes\n");
+            printf("Please enter xxx.nes\n");
             goto error;
         }
     }else{
-        nes_printf("Please enter the nes file path\n");
+        printf("Please enter the nes file path\n");
         goto error;
     }
 error:
